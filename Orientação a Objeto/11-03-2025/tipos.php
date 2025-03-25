df9756f94789f89a5923dbad13a36dca349cd293
Maycon J. G. de Santana <mayconjgs.ifpr2024@gmail.com>
2025-03-25 10:26:36 -0300

    Update and rename Orientação a Objeto/tipos.php to Orientação a Objeto/11-03-2025/tipos.php

---
+++ Orientação a Objeto/11-03-2025/tipos.php
@@ -0,0 +1,21 @@
+<?php
+
+$n1 = 23.4;
+$n2 = 11;
+$soma = $n1 + $n2;
+
+echo "a soma de $n1 + $n2 é igual a $soma \n";
+
+echo "a soma de n1 + n2 é igual: " . $soma . "\n";
+
+// Concantenamos pois alguns programas não aceitam a união de variáveis dentre textos simples
+
+$tipo = gettype($soma);
+
+// "gettype" -> pega o tipo do informado
+
+echo $tipo . "\n";
+
+
+
+
