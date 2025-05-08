eabed9605ba004facd81275eb52b3aa0c960e1c1
Maycon J. G. de Santana <mayconjgs.ifpr2024@gmail.com>
2025-05-08 08:19:58 -0300

    Update ex2_mine.php

--- Orientação a Objeto/1-bimestre/05-06-2025/conteudo/slides/ex2_mine.php
+++ Orientação a Objeto/1-bimestre/05-06-2025/conteudo/slides/ex2_mine.php
@@ -5,8 +5,44 @@ class Retangulo {
     public $altura;
     public $base;
 
+
+    function __construct ($altura, $base) {
+
+        $this -> altura = $altura;
     
-    function area() {
+        $this -> base = $base;
+
+    }
+
+    
+    function area() : float {
+
         $area = $this -> altura * $this -> base;
+
+        return $area;
+
+    }
+
+    function perimetro() : float {
+
+        $perimetro = $this -> altura * 2 + $this -> base * 2;
+
+        return $perimetro;
+
     }
 }
+
+for ($i = 1; $i <= 3; $i++) {
+
+    $altura = readline("Qual a altura do seu retângulo? ");
+    
+    $base = readline("Qual o tamanho da base do seu retângulo? ");
+
+    $retangulo = new Retangulo($altura, $base);
+
+    print("A àrea do seu retângulo é: " . $retangulo -> area() . "\n");
+
+    print("O perímetro do seu retângulo é: " . $retangulo -> perimetro() . "\n");
+
+}
+
