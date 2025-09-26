ae92d6c42135b07ac3b552eb09a36f9851622704
Maycon J. G. de Santana <mayconjgs.ifpr2024@gmail.com>
2025-09-26 14:56:11 +0000

    Exercicios JavaScript

---
+++ Desenvolvimento Web/3-bimestre/09-26-2025/exercicios/2./noticia.js
@@ -0,0 +1,8 @@
+let tituloNoticia = document.querySelectorAll('.post__title')[0];
+let subTituloNoticia = document.querySelectorAll('.post__title')[1];
+let imagemNoticia = document.querySelectorAll('.post__image img')[0];
+
+imagemNoticia.srcset = "https://etimologia.com.br/foto/filosofia/Paz-simbolo-passaro.jpg";
+
+tituloNoticia.textContent = "Enfim paz: todos seres humanos trabalhando coletivamente";
+subTituloNoticia.textContent = "As pessoas perceberam que todas buscavam o mesmo objetivo. Estudos apontam que a motivação para guerras era, na verdade, a interpretação equivocada"
