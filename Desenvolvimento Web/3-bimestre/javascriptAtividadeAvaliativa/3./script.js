2514675755acc5c4fa47d1d3f0dc347ebee78a2a
Maycon J. G. de Santana <mayconjgs.ifpr2024@gmail.com>
2025-10-17 14:54:51 +0000

    share Link

--- Desenvolvimento Web/3-bimestre/javascriptAtividadeAvaliativa/3./script.js
+++ Desenvolvimento Web/3-bimestre/javascriptAtividadeAvaliativa/3./script.js
@@ -1,8 +1,16 @@
 let botaoMuda = document.querySelector("#botaoMuda")
-let titulo = document.querySelector("#titulo");
+let titulo = document.querySelector("#titulo")
+let textos = ["Sinta...", "Adore...", "Proclame...", "Afirme...", "Defenda...", "Imagine...",
+              "Sonhe...", "Agradeça...", "Pense...", "Aja...", "Viva...", "Mantenha...",
+              "Equilibre...", "Fique...", "Excite...", "Felicite...", "Crie...", "Assista...",
+              "Ajude...", "Exista...", "Converse...", "Escreva...", "Durma...", "Descanse...",
+              "Produza...", "Observa...", "Garanta...", "Determine...", "Esclareça...",
+              "Tente...", "Pegue..."]
 
 botaoMuda.addEventListener("click", () => {
 
-    titulo.style.fontSize = "2px"
-    
+    titulo.style.fontSize = 30 + Math.random() * (100 - 30) + "px"
+    titulo.style.color = 'rgb(' + (Math.random() * 255) + ', ' +  (Math.random() * 255) + ', ' + (Math.random() * 255) + ')'
+    titulo.innerText = textos[Math.floor(Math.random() * textos.length)]
+
 })
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # arquivos-2025
 
 Repositório utilizado para armazenar unicamente atividades que precisam ser compartilhadas com os professores.
+
+https://prod.liveshare.vsengsaas.visualstudio.com/join?E01165B81FAFAE76A982DD3BADF7D4AD57D8
