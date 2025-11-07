277fe9a92372c58076d31456bebce6cac7d3ad61
Maycon J. G. de Santana <mayconjgs.ifpr2024@gmail.com>
2025-11-07 12:32:14 +0000

    add Execucao Exercicio  Calculator

---
+++ Orientação a Objeto/4-bimestre/11-07-2025/folhinha1/ex1/execucao.php
@@ -0,0 +1,36 @@
+require_once(“model/Sum”);
+
+options = [“Soma”, “Subtração”, “Multiplicação”, “Divisão”, “Resto”];
+
+while (true) {
+
+while (true) {
+
+$choice = readline(“Qual sua escolha?  “);
+
+If ($choice < 0 || $choice > count($options)) {
+
+print(“Valor inválido!”);
+
+}
+else {
+
+break;
+
+}
+
+}
+
+
+
+if ($operation -> getNumA() == 0 && $operation -> getNumB() == 0)
+
+exit();
+
+else {
+
+}
+}
+else {
+
+}
